362fff1379924c4e3e5bccc2e7d176126d8e7596
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-07 13:50:17 +0200

    Add MovieDTO

---
+++ Movies-DAL/DTO/Movies/MovieDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies_DAL.DTO.Movies
+{
+    public class MovieDTO
+    {
+        public int Id { get; set; }
+        public string ImdbId { get; set; }
+        public string Title { get; set; }
+        public string CoverUrl { get; set; }
+        public int Year { get; set; }
+        public string OriginalAirDate { get; set; }
+        public string Kind { get; set; }
+        public decimal Rating { get; set; }
+        public string Plot { get; set; }
+        public int Top250Rank { get; set; }
+    }
+}
